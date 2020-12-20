dd019bec7754d19914df9196b120f4f0be91f123
Isabella Thando_N <65547230+ThandoNdlovu@users.noreply.github.com>
2020-12-20 15:17:11 +0200

    Update Navigation.php

--- Purebliss m/Pages/Navigation.php
+++ Purebliss m/Pages/Navigation.php
@@ -4,8 +4,8 @@
 <a href="#" class="logo"><img src="../Images/title-icon.png" alt="icon" height="40px" weight="40px">Pure bliss M</a>
 <!--  menu  -->
 <ul>
-<li><a href="Home.php" class="active">Home</a></li>
-<li><a href="Home.php">About</a></li>
+<li><a href="index.php" class="active">Home</a></li>
+<li><a href="index.php">About</a></li>
 <li><a href="Services.php">Services</a></li>
 <li><a href="Gallary.php">Gallary</a></li>
 <li><a href="#footer">Contact Us</a></li>
